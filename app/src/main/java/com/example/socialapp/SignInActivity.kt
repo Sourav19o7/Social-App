ec51254f97d894f20742f17f30729170084fd8b7
Sourav <sourav.dey0147@gmail.com>
2022-07-19 22:40:55 +0530

    initial commit

---
+++ app/src/main/java/com/example/socialapp/SignInActivity.kt
@@ -0,0 +1,23 @@
+package com.example.socialapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        val signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            .build()
+    }
+}
